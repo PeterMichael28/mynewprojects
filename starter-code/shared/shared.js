a81ce0dec44e13f21714acf3b1fed82c71ced0f4
peter michael <enitanpeters28@gmail.com>
2022-05-01 17:46:08 -1200

    my space landing page

---
+++ starter-code/shared/shared.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn-container");
+const nav = document.querySelector(".nav-ul");
+
+
+
+function myFunction() {
+    btn.classList.toggle("change");
+    nav.classList.toggle("change");
+}
